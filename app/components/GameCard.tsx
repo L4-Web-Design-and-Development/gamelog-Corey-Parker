2b040b9e31796efa2ee03cc8b06cf2ff62c341c7
Corey Parker <238495322+Corey-Parker@users.noreply.github.com>
2026-03-05 15:40:19 +0000

    Updated placeholder image, updated query to include genre, updated GameCard component

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,16 +1,21 @@
 interface GameCardProps {
   title: string;
   releaseDate: string;
+  category: string;
 }
 
-export default function GameCard({ title, releaseDate }: GameCardProps) {
+export default function GameCard({
+  title,
+  releaseDate,
+  category,
+}: GameCardProps) {
   const formattedDate = releaseDate.substring(0, 10);
 
   return (
     <div className="flex flex-col gap-4">
       <div className="relative h-72 overflow-hidden">
         <img
-          src="https://picsum.photos/400/300"
+          src="app\assets\Png\Hornet.png"
           alt="Game Cover"
           className="w-full object-cover rounded-md"
         />
@@ -18,7 +23,9 @@ export default function GameCard({ title, releaseDate }: GameCardProps) {
       <div className="flex justify-between">
         <div className="flex flex-col justify-between w-2/3">
           <h3 className="font-bold text-2xl text-slate-300">{title}</h3>
-          <p className="text-cyan-300 uppercase text-sm font-semibold">Genre</p>
+          <p className="text-cyan-300 uppercase text-sm font-semibold">
+            {category}
+          </p>
           <p className="text-slate-200/60 text-sm font-semibold">
             {formattedDate}
           </p>
