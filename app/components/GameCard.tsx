99056fc38f121da4e860262bc1404aad8dce39c2
Corey Parker <238495322+Corey-Parker@users.noreply.github.com>
2026-04-30 14:09:22 +0100

    Basic tweaking for css, added component for top of page bar

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -26,25 +26,44 @@ export default function GameCard({
           className="w-full object-cover rounded-md"
         />
       </div>
+
       <div className="flex justify-between">
-        <div className="flex flex-col justify-between w-2/3">
-          <h3 className="font-bold text-2xl text-slate-300">{title}</h3>
-          <p className="text-cyan-300 uppercase text-sm font-semibold">
-            {category}
-          </p>
-          <p className="text-slate-200/60 text-sm font-semibold">
-            {formattedDate}
-          </p>
+        <div className="flex flex-col justify-between w-2/3 ">
+          <div>
+            <div>
+              <h3 className="font-bold text-2xl text-slate-300">{title}</h3>
+            </div>
+            <div>
+              <p className="text-cyan-300 uppercase text-sm font-semibold">
+                {category}
+              </p>
+            </div>
+            <div>
+              <p className="text-slate-200/60 text-sm font-semibold">
+                {formattedDate}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="space-y-8">
+          <Form action={`/delete-edit/${gameID}`} method="post">
+            <button
+              type="submit"
+              className="border-2 border-green-300 text-green-300  pl-10 pr-10 rounded-md transition hover:bg-red-50/10 w-full"
+            >
+              Edit
+            </button>
+          </Form>
+          <Form action={`/delete-game/${gameID}`} method="post">
+            <button
+              type="submit"
+              className="border-2 border-red-300 text-red-300  rounded-md transition hover:bg-red-50/10  w-full"
+            >
+              Delete
+            </button>
+          </Form>
         </div>
       </div>
-      <Form action={`/delete-game/${gameID}`} method="post">
-        <button
-          type="submit"
-          className="border-2 border-red-300 text-red-300 p-2 rounded-md transition hover:bg-red-50/10 w-full"
-        >
-          Delete
-        </button>
-      </Form>
     </div>
   );
 }
---
+++ app/components/TopBar.tsx
@@ -0,0 +1,9 @@
+import logo from "app\\assets\\svg\\logo-full.svg";
+
+export default function TopBar({}) {
+  return (
+    <div>
+      <img src={logo} alt=""></img>
+    </div>
+  );
+}
